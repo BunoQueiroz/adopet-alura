61b6ba3bb4ebde2981b4137e3788de0bcdeea0fb
Bruno_Castro_Queiroz <bs8872491@gmail.com>
2023-03-29 15:12:01 -0300

    Criando as rotas da api

--- config/urls.py
+++ config/urls.py
@@ -1,6 +1,12 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
+from tutor.views import TutorViewSet
+
+router = routers.DefaultRouter()
+router.register('tutors', TutorViewSet, 'tutors')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include(router.urls)),
 ]
