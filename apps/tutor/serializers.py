a6c41cf4be040f886f64b51fbe5cdeca36b00482
Bruno_Castro_Queiroz <bs8872491@gmail.com>
2023-03-30 15:21:36 -0300

    Adicionando o validador de email à classe serializaer de tutor e especificando os campos em que as mensagens de erro serão exibidas

--- apps/tutor/serializers.py
+++ apps/tutor/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 from tutor.models import Tutor
-from .validators import different_passwords, weak_password, full_name_invalid
+from .validators import different_passwords, weak_password, full_name_invalid, email_invalid
 
 class TutorSerializer(serializers.ModelSerializer):
     
@@ -8,13 +8,19 @@ class TutorSerializer(serializers.ModelSerializer):
     
     class Meta:
         model = Tutor
-        fields = ['full_name', 'email', 'password', 'confirm_password']
+        fields = ['id', 'full_name', 'email', 'password', 'confirm_password']
     
     def validate(self, data):
         if weak_password(data['password']):
-            raise serializers.ValidationError('A senha é muito fraca')
+            raise serializers.ValidationError({'password': 'A senha é muito fraca'})
+        
         if different_passwords(data['password'], data['confirm_password']):
-            raise serializers.ValidationError('As senhas passadas NÃO podem ser diferentes')
+            raise serializers.ValidationError({'confirm_password': 'As senhas passadas NÃO podem ser diferentes'})
+        
         if full_name_invalid(data['full_name']):
-            raise serializers.ValidationError('O nome passado não é válido')
+            raise serializers.ValidationError({'full_name': 'O nome passado não é válido'})
+        
+        if email_invalid(data['email']):
+            raise serializers.ValidationError({'email': 'Endereço de email inválido'})
+
         return data
