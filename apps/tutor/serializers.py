173b4ab8be2e37e97ddcdc5d5d84b50a5b9a94a9
Bruno_Castro_Queiroz <bs8872491@gmail.com>
2023-03-30 08:49:42 -0300

    Validando as senhas dos tutores de acordo com as funções de validators.py

--- apps/tutor/serializers.py
+++ apps/tutor/serializers.py
@@ -1,7 +1,18 @@
 from rest_framework import serializers
 from tutor.models import Tutor
+from .validators import different_passwords, weak_password
 
 class TutorSerializer(serializers.ModelSerializer):
+    
+    confirm_password = serializers.CharField(write_only=True)
+    
     class Meta:
         model = Tutor
-        fields = ['full_name', 'email', 'password']
+        fields = ['full_name', 'email', 'password', 'confirm_password']
+    
+    def validate(self, data):
+        if weak_password(data['password']):
+            raise serializers.ValidationError('A senha é muito fraca')
+        if different_passwords(data['password'], data['confirm_password']):
+            raise serializers.ValidationError('As senhas passadas NÃO podem ser diferentes')
+        return data
