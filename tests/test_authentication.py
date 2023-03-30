f93c5e303ec128f34ce47de7e3beddf7fdab7e03
Bruno_Castro_Queiroz <bs8872491@gmail.com>
2023-03-30 08:44:13 -0300

    Desenvolvendo testes de altenticação de usuários não cadastrados e adicionando docstrings aos testes ja implementados

--- tests/test_authentication.py
+++ tests/test_authentication.py
@@ -9,11 +9,27 @@ class AuthenticationTestCase(TestCase):
         self.user = User.objects.create_user('bruno', 'b@gmail.com', 'bruno')
         self.tutor_one = Tutor.objects.create_user(full_name='fernando henrique cardoso', username='fernando', password='fernando')
         
-    def test_authenticacao_basica_para_os_tutores(self):
+    def test_basic_authentication_for_tutors(self):
+        """Verifica se as autenticações estão ocorrendo de maneira correta com os tutores cadastrados"""
         result = authenticate(username='fernando', password='fernando')
         self.assertIsNotNone(result)
 
-    def test_authenticacao_usuarios_no_geral(self):
+    def test_authentication_users_in_general(self):
+        """Verifica se as autenticações de usuários (sem nomenclatura específica) estão acontecendo corretamente"""
         result = authenticate(username='bruno', password='bruno')
         self.assertIsNotNone(result)
     
+    def test_unregistered_tutor_authentication(self):
+        """Verifica se as autenticações de tutores não cadastrados não estão ocorrendo"""
+        tutor_result = authenticate(username='tutor', password='tutor1234')
+        null_result = authenticate(username='', password='')
+        self.assertIsNone(tutor_result)
+        self.assertIsNone(null_result)
+        
+    def test_unregistered_users_authentication(self):
+        """Verifica se as autenticações de usuários não cadastrados não ocorrão de forma alguma"""
+        admin_result = authenticate(username='admin', password='admin1234')
+        null_result = authenticate(username='', password='')
+        self.assertIsNone(admin_result)
+        self.assertIsNone(null_result)
+
