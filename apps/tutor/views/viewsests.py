9f8a007bf38b9a00e5e3cac2b5ed9db94833d68b
Bruno_Castro_Queiroz <bs8872491@gmail.com>
2023-03-31 11:47:12 -0300

    Reescrevendo a função update da viewset de tutor para as senhas serem alteradas corretamente

--- apps/tutor/views/viewsests.py
+++ apps/tutor/views/viewsests.py
@@ -2,6 +2,7 @@ from rest_framework import viewsets, status
 from tutor.models import Tutor
 from tutor.serializers import TutorSerializer
 from rest_framework.response import Response
+from django.shortcuts import get_object_or_404
 
 class TutorViewSet(viewsets.ModelViewSet):
     serializer_class = TutorSerializer
@@ -13,3 +14,14 @@ class TutorViewSet(viewsets.ModelViewSet):
         headers = self.get_success_headers(serializer.data)
         Tutor.objects.create_user(full_name=request.POST['full_name'], username=request.POST['full_name'], email=request.POST['email'], password=request.POST['password'])
         return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+
+    def update(self, request, *args, **kwargs):
+        instance = self.get_object()
+        serializer = self.get_serializer(instance, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_update(serializer)
+        
+        t = get_object_or_404(Tutor, email=request.data['email'])
+        t.set_password(request.data['password'])
+        t.save()
+        return Response(serializer.data, status=status.HTTP_200_OK)
