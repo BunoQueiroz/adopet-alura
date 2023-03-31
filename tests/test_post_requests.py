2fdee22b75727e67eac067d767a5a68b5c6dd095
Bruno_Castro_Queiroz <bs8872491@gmail.com>
2023-03-31 16:25:24 -0300

    Desenvolvendo testes para requisições POST

---
+++ tests/test_post_requests.py
@@ -0,0 +1,24 @@
+from rest_framework.test import APITestCase
+from tutor.models import Tutor
+from rest_framework import status
+
+class POSTRequestsTestCase(APITestCase):
+    def setUp(self):
+        self.data = {
+            'full_name': 'tutor test post',
+            'email': 'new-email@email.com',
+            'password': 'NewPassword01',
+            'confirm_password': 'NewPassword01',
+        }
+        self.tutor_one = Tutor.objects.create_user('test post', 'post@email.com', 'testpost01', full_name='test post', pk=1)
+
+    def test_request_new_tutor_status_200(self):
+        """Verifica se a criação de novos tutores devolve STATUS 200"""
+        request = self.client.post('/tutors/', self.data)
+        self.assertEqual(request.status_code, status.HTTP_201_CREATED)
+    
+    def test_new_tutor_created(self):
+        """Verifica se a criação de novos tutores está ocorrendo de fato no banco de dados"""
+        self.client.post('/tutors/', self.data)
+        new_tutor_expected = Tutor.objects.filter(email='new-email@email.com').exists()
+        self.assertTrue(new_tutor_expected)
