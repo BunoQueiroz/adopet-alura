77c2c3195541fe226f4bf74c3e5eafd15dff1363
Bruno_Castro_Queiroz <bs8872491@gmail.com>
2023-04-03 16:41:18 -0300

    Registrando uma rota para os recursos de pet

--- config/urls.py
+++ config/urls.py
@@ -2,9 +2,11 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework import routers
 from tutor.views import TutorViewSet
+from pet.views import PetViewSet
 
 router = routers.DefaultRouter()
 router.register('tutors', TutorViewSet, 'tutors')
+router.register('pets', PetViewSet, 'pets')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
