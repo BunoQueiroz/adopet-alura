16b54d3c52282b76ff07d5f2dc2c7fba02712b83
Bruno_Castro_Queiroz <bs8872491@gmail.com>
2023-04-04 07:20:02 -0300

    Criando uma rota para o recurso de abrigo

--- config/urls.py
+++ config/urls.py
@@ -3,10 +3,12 @@ from django.urls import path, include
 from rest_framework import routers
 from tutor.views import TutorViewSet
 from pet.views import PetViewSet
+from shelter.views import ShelterViewSet
 
 router = routers.DefaultRouter()
 router.register('tutors', TutorViewSet, 'tutors')
 router.register('pets', PetViewSet, 'pets')
+router.register('shelters', ShelterViewSet, 'shelters')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
