f347454849e8370caf5734e8f1ed2943c835799b
Bruno_Castro_Queiroz <bs8872491@gmail.com>
2023-04-04 09:02:52 -0300

    Aplicando a validação de nome no serializer de abrigo

--- apps/shelter/serializers.py
+++ apps/shelter/serializers.py
@@ -1,7 +1,14 @@
 from rest_framework import serializers
 from shelter.models import Shelter
+from shelter.validators import invalid_name_shelter
 
 class ShelterSerializer(serializers.ModelSerializer):
     class Meta:
         model = Shelter
         fields = '__all__'
+
+    def validate(self, data):
+        if invalid_name_shelter(data['name']):
+            raise serializers.ValidationError({'name': 'O nome deve conter ao menos uma letra e não pode possuir espaços antes ou depois dos caracteres que o seu nome necessitam'})
+        return data
+    
