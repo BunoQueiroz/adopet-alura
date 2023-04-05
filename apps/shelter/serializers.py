831b1c5c7baa732ac4a5e90124b3b06474213311
Bruno_Castro_Queiroz <bs8872491@gmail.com>
2023-04-05 09:48:35 -0300

    Aplicando as validações de nome de cidade no serializer de abrigo

--- apps/shelter/serializers.py
+++ apps/shelter/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 from shelter.models import Shelter
-from shelter.validators import invalid_name_shelter
+from shelter.validators import invalid_name_shelter, invalid_name_city
 
 class ShelterSerializer(serializers.ModelSerializer):
     class Meta:
@@ -9,6 +9,9 @@ class ShelterSerializer(serializers.ModelSerializer):
 
     def validate(self, data):
         if invalid_name_shelter(data['name']):
-            raise serializers.ValidationError({'name': 'O nome deve conter ao menos uma letra e não pode possuir espaços antes ou depois dos caracteres que o seu nome necessitam'})
+            raise serializers.ValidationError({'name': 'O nome deve conter ao menos uma letra'})
+        
+        if invalid_name_city(data['city']):
+            raise serializers.ValidationError({'city': 'Nome de cidade inválido'})
+
         return data
-    
