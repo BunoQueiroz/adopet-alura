ace393e2cecc853aba133c94e26a388a3931dec3
Bruno_Castro_Queiroz <bs8872491@gmail.com>
2023-04-05 11:41:26 -0300

    Desenvolvendo testes para os nomes das ruas brasileiras serem validadas corretamente

---
+++ tests/shelter/validators/test_valid_road.py
@@ -0,0 +1,45 @@
+from rest_framework.test import APITestCase
+from shelter.serializers import ShelterSerializer
+
+class ShelterRoadValidTestCase(APITestCase):
+    def setUp(self) -> None:
+        self.road_specials_characters = ShelterSerializer(data={
+            'name': 'Shelter@ & Valid-123ração',
+            'road': 'Ru@ inválida',
+            'number': 1,
+            'borhood': 'Borhood',
+            'CEP': '65548-123',
+            'city': 'city valid',
+            'state': 'state',
+        })
+        self.road_graphic_accents = ShelterSerializer(data={
+            'name': 'Shelter@ & Valid-123ração',
+            'road': 'Rua João Fantástico Uchôa',
+            'number': 1,
+            'borhood': 'Borhood',
+            'CEP': '65548-123',
+            'city': 'city valid',
+            'state': 'state',
+        })
+        self.road_number_in_name = ShelterSerializer(data={
+            'name': 'Shelter@ & Valid-123ração',
+            'road': 'Rua João Fantástico Uchôa',
+            'number': 1,
+            'borhood': 'Borhood',
+            'CEP': '65548-123',
+            'city': 'city 085 valid',
+            'state': 'state',
+        })
+
+    def test_specials_characters_in_road(self):
+        """Impossível haver ruas brasileiras com certos caracteres especiais"""
+        self.assertFalse(self.road_specials_characters.is_valid())
+
+    def test_road_graphic_accents(self):
+        """Permite a acentuação gráfica nos nomes das ruas (braileiras)"""
+        self.assertTrue(self.road_graphic_accents.is_valid())
+    
+    def test_number_in_road_name(self):
+        """Permite a utilização de números nos nomes das ruas (braileiras)"""
+        self.assertTrue(self.road_graphic_accents.is_valid())
+    
