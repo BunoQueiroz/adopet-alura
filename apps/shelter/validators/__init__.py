d02cff026cd1943dfab3324d773bef623fbbe404
Bruno_Castro_Queiroz <bs8872491@gmail.com>
2023-04-05 11:12:50 -0300

    Mantendo padrão de nomenclatura entre os validadores no app de abrigo

--- apps/shelter/validators/__init__.py
+++ apps/shelter/validators/__init__.py
@@ -1,3 +1,3 @@
 from .shelter_name_validator import invalid_name_shelter
 from .city_name_invalid import invalid_name_city
-from .cep_invalid import invalid_format_cep
+from .cep_validator import invalid_format_cep
