37ad535f59d21c1ddc0b13e44b46105528395e20
Bruno_Castro_Queiroz <bs8872491@gmail.com>
2023-04-05 11:50:22 -0300

    Aplicando as validações de nome de rua no serializer de abrigo

--- apps/shelter/serializers.py
+++ apps/shelter/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 from shelter.models import Shelter
-from shelter.validators import invalid_name_shelter, invalid_name_city, invalid_format_cep
+from shelter.validators import invalid_name_shelter, invalid_name_city, invalid_format_cep, invalid_name_road
 
 class ShelterSerializer(serializers.ModelSerializer):
     class Meta:
@@ -16,5 +16,8 @@ class ShelterSerializer(serializers.ModelSerializer):
 
         if invalid_format_cep(data['CEP']):
             raise serializers.ValidationError({'CEP': 'CEP de formato inválido. Siga o formato: xxxxx-xxx'})
+
+        if invalid_name_road(data['road']):
+            raise serializers.ValidationError({'road': 'Rua não prevista em nossas análises'})
 
         return data
