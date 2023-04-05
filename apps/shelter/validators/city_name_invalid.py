15d892c533fc82dcb1f37d7786f7681738fbe440
Bruno_Castro_Queiroz <bs8872491@gmail.com>
2023-04-05 11:38:04 -0300

    Adicionando acentuação gráfica à regex de nome de cidades válidas

--- apps/shelter/validators/city_name_invalid.py
+++ apps/shelter/validators/city_name_invalid.py
@@ -1,5 +1,5 @@
 from re import match
 
 def invalid_name_city(city: str) -> bool:
-    pattern = r'^[a-zA-Z_\s-]{4,}$'
+    pattern = r'^[a-zA-Z_çãáàõôóúûí\s-]{4,}$'
     return not match(pattern, city)
