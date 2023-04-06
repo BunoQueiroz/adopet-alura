26d2749ba9043ed9fbf71235024e9ce169f25c2c
Bruno_Castro_Queiroz <bs8872491@gmail.com>
2023-04-06 15:51:24 -0300

    Desenvolvendo testes para validar apropriadamentes os números no app de abrigo

---
+++ tests/shelter/validators/test_valid_number.py
@@ -0,0 +1,57 @@
+from rest_framework.test import APITestCase
+from shelter.serializers import ShelterSerializer
+
+class ShelterBorhoodValidTestCase(APITestCase):
+    def setUp(self) -> None:
+        self.number_with_letter = ShelterSerializer(data={
+            'name': 'Nome do Abrigo',
+            'road': 'Rua Válida',
+            'number': '1n0',
+            'borhood': 'Boa Vista',
+            'CEP': '62685-000',
+            'city': 'Paraipaba',
+            'state': 'CE',
+        })
+        self.number_negative = ShelterSerializer(data={
+            'name': 'Nome do Abrigo',
+            'road': 'Rua Válida',
+            'number': -1,
+            'borhood': 'Boa Vista',
+            'CEP': '62685-000',
+            'city': 'Paraipaba',
+            'state': 'CE',
+        })
+        self.number_greater_then_9999 = ShelterSerializer(data={
+            'name': 'Nome do Abrigo',
+            'road': 'Rua Válida',
+            'number': 10000,
+            'borhood': 'Boa Vista',
+            'CEP': '62685-000',
+            'city': 'Paraipaba',
+            'state': 'CE',
+        })
+        self.number_float = ShelterSerializer(data={
+            'name': 'Nome do Abrigo',
+            'road': 'Rua Válida',
+            'number': 1.1,
+            'borhood': 'Boa Vista',
+            'CEP': '62685-000',
+            'city': 'Paraipaba',
+            'state': 'CE',
+        })
+
+    def test_number_in_string_format(self):
+        """Não são permitidos NÚMEROS juntamente com letras"""
+        self.assertFalse(self.number_with_letter.is_valid())
+    
+    def test_negative_number(self):
+        """Não são permitidos NÚMEROS menores que 0"""
+        self.assertFalse(self.number_negative.is_valid())
+    
+    def test_very_big_number(self):
+        """Não são permitidos NÚMEROS que excedam 4 dígitos"""
+        self.assertFalse(self.number_greater_then_9999.is_valid())
+
+    def test_float_number(self):
+        """Não são permitidos NÚMEROS de ponto flutuante"""
+        self.assertFalse(self.number_float.is_valid())
