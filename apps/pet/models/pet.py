f9078ba3e2564e8d462f33670d973c0d06507a62
Bruno_Castro_Queiroz <bs8872491@gmail.com>
2023-04-12 10:33:34 -0300

    Permitindo que o campo de imagem dos petes não sejam passados obrigatóriamente

--- apps/pet/models/pet.py
+++ apps/pet/models/pet.py
@@ -3,7 +3,7 @@ from shelter.models import Shelter
 
 class Pet(models.Model):
     name = models.CharField(max_length=20)
-    image = models.ImageField(upload_to='pet/%Y/%m/%d/')
+    image = models.ImageField(upload_to='pet/%Y/%m/%d/', blank=True)
     age = models.CharField(max_length=10)
     size = models.CharField(max_length=15)
     characteristics = models.CharField(max_length=25)
