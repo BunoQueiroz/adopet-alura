738498a4d99000d78b14f68d0785ff8ab00510c6
Bruno_Castro_Queiroz <bs8872491@gmail.com>
2023-04-12 11:15:46 -0300

    Adicionando a validação de nome de pet ao serializer de pet

--- apps/pet/serializers.py
+++ apps/pet/serializers.py
@@ -1,7 +1,15 @@
 from rest_framework import serializers
 from pet.models import Pet
+from pet.validators import invalid_pet_name
 
 class PetSerializer(serializers.ModelSerializer):
     class Meta:
         model = Pet
         fields = '__all__'
+
+    def validate(self, data):
+        
+        if invalid_pet_name(data['name']):
+            raise serializers.ValidationError({'name': 'Nome de pet inválido'})
+        
+        return data
