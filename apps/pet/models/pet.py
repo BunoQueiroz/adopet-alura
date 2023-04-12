28e0ffca8a4355a1ba07988eedb8121ca469ca82
Bruno_Castro_Queiroz <bs8872491@gmail.com>
2023-04-12 10:12:23 -0300

    Adicionando o campo de imagem aos modelos de pet

--- apps/pet/models/pet.py
+++ apps/pet/models/pet.py
@@ -3,6 +3,7 @@ from shelter.models import Shelter
 
 class Pet(models.Model):
     name = models.CharField(max_length=20)
+    image = models.ImageField(upload_to='pet/%Y/%m/%d/')
     age = models.CharField(max_length=10)
     size = models.CharField(max_length=15)
     characteristics = models.CharField(max_length=25)
