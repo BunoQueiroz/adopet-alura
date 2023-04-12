7cd84a5707b1983351285f7f0010d4ed1367bcec
Bruno_Castro_Queiroz <bs8872491@gmail.com>
2023-04-12 15:20:40 -0300

    Adicionando a validação do campo de idade ao serializer de pet

--- apps/pet/serializers.py
+++ apps/pet/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 from pet.models import Pet
-from pet.validators import invalid_pet_name
+from pet.validators import invalid_pet_name, invalid_age_pet
 
 class PetSerializer(serializers.ModelSerializer):
     class Meta:
@@ -12,4 +12,7 @@ class PetSerializer(serializers.ModelSerializer):
         if invalid_pet_name(data['name']):
             raise serializers.ValidationError({'name': 'Nome de pet inválido'})
         
+        if invalid_age_pet(data['age']):
+            raise serializers.ValidationError({'age': 'Idade inválida'})
+
         return data
