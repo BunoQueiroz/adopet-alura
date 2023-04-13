be4c77545bc3a93e2decf6e03753607df5c6100f
Bruno_Castro_Queiroz <bs8872491@gmail.com>
2023-04-13 17:22:51 -0300

    Refatorando a função de validação de CEP para permitir os CEPs em branco

--- apps/shelter/validators/cep_validator.py
+++ apps/shelter/validators/cep_validator.py
@@ -1,5 +1,7 @@
 from re import match
 
 def invalid_format_cep(cep: str) -> bool:
-    pattern = r'^\d{5}-\d{3}$'
-    return not match(pattern, cep)
+    if cep:
+        pattern = r'^\d{5}-\d{3}$'
+        return not match(pattern, cep)
+    return False
