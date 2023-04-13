bf18b13bb958497dc5d1c15c6d7aaa5df91442e6
Bruno_Castro_Queiroz <bs8872491@gmail.com>
2023-04-13 15:23:19 -0300

    Validando ruas que não forem passadas

--- apps/shelter/validators/road_validator.py
+++ apps/shelter/validators/road_validator.py
@@ -1,5 +1,7 @@
 from re import match
 
 def invalid_name_road(road: str) -> bool:
-    pattern = r'^[a-zA-Z0-9çãàáíîóôú\s_-]{4,}$'
-    return not match(pattern, road)
+    if road:
+        pattern = r'^[a-zA-Z0-9çãàáíîóôú\s_-]{4,}$'
+        return not match(pattern, road)
+    return False
