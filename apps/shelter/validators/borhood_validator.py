7f67115aad9037b377fd7c9ac3eb8185d37740ee
Bruno_Castro_Queiroz <bs8872491@gmail.com>
2023-04-14 10:40:22 -0300

    Realizando as auterações para bairros em branco serem válidos de fato

--- apps/shelter/validators/borhood_validator.py
+++ apps/shelter/validators/borhood_validator.py
@@ -1,5 +1,7 @@
 from re import match
 
 def invalid_borhood(borhood: str) -> bool:
-    pattern = r'^[a-zA-Z\s]{3,}$'
-    return not match(pattern, borhood)
+    if borhood:
+        pattern = r'^[a-zA-Z\s]{3,}$'
+        return not match(pattern, borhood)
+    return False
