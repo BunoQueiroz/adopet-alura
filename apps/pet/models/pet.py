cefa706f26f0a93c1c93d62002fab919e3e1d5a5
Bruno_Castro_Queiroz <bs8872491@gmail.com>
2023-04-14 10:20:00 -0300

    Criando uma classe para adoção de pets

--- apps/pet/models/pet.py
+++ apps/pet/models/pet.py
@@ -1,5 +1,7 @@
 from django.db import models
 from shelter.models import Shelter
+from tutor.models import Tutor
+from datetime import date
 
 class Pet(models.Model):
     name = models.CharField(max_length=20)
@@ -11,3 +13,12 @@ class Pet(models.Model):
 
     def __str__(self):
         return self.name
+
+class Adocao(models.Model):
+    pet = models.ForeignKey(Pet, on_delete=models.CASCADE)
+    tutor = models.ForeignKey(Tutor, on_delete=models.CASCADE)
+    date = models.DateField(default=date.today())
+
+    def __str__(self) -> str:
+        return f'{self.tutor} -> {self.pet}'
+    
