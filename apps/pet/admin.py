4bb8fb86e66d6afbb82de4f3008e8579b2847f14
Bruno_Castro_Queiroz <bs8872491@gmail.com>
2023-04-14 10:25:19 -0300

    Registrando os modelos de abrigo, pet e adoção no Admin do Django

--- apps/pet/admin.py
+++ apps/pet/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from pet.models import Adocao, Pet
 
-# Register your models here.
+admin.site.register(Adocao)
+admin.site.register(Pet)
--- apps/shelter/admin.py
+++ apps/shelter/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
+from shelter.models import Shelter
 
-# Register your models here.
+admin.site.register(Shelter)
