dd0d9973aa9a1e8a08e6df5db84bc47fdfe71a5e
Bruno_Castro_Queiroz <bs8872491@gmail.com>
2023-04-16 07:37:24 -0300

    Alterando a data defualt do modelo de pet

--- apps/pet/models/adoption.py
+++ apps/pet/models/adoption.py
@@ -1,12 +1,12 @@
 from django.db import models
 from tutor.models import Tutor
-from datetime import date
+from django.utils import timezone
 from pet.models import Pet
 
 class Adoption(models.Model):
     pet = models.ForeignKey(Pet, on_delete=models.CASCADE)
     tutor = models.ForeignKey(Tutor, on_delete=models.CASCADE)
-    date = models.DateField(default=date.today())
+    date = models.DateField(default=timezone.datetime.today().date().__format__('%d-%m-%Y'))
 
     def __str__(self) -> str:
         return f'{self.tutor} -> {self.pet}'
