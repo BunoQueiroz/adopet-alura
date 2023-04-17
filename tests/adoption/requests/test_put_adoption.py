3a409cd466e3f08c4a69df5b599796d76f65011d
Bruno_Castro_Queiroz <bs8872491@gmail.com>
2023-04-17 08:02:41 -0300

    Removendo código extra no teste de put -> adoção

--- tests/adoption/requests/test_put_adoption.py
+++ tests/adoption/requests/test_put_adoption.py
@@ -10,7 +10,7 @@ class AdoptionPUTRequestsTestCase(APITestCase):
         shelter = Shelter.objects.create(id=1, name='Meu abrigo', city='Paraipaba', state='CE')
         pet = Pet.objects.create(id=1, name='Meu pet', size='Médio', age='2 meses', shelter=shelter, characteristics='Ágil e fiel')
         tutor = Tutor.objects.create(id=1, full_name='Bruno castro', email='email@gmail.com', password='senha1234')
-        self.adoption = Adoption.objects.create(id=1, pet=pet, tutor=tutor, date='2023-05-02')
+        Adoption.objects.create(id=1, pet=pet, tutor=tutor, date='2023-05-02')
         self.data_in_db = {'pet': 1, 'tutor': 1, 'date': '2023-05-03'}
 
     def test_adoption_put_status_405(self):
@@ -18,7 +18,7 @@ class AdoptionPUTRequestsTestCase(APITestCase):
         response = self.client.put('/adoptions/1/', self.data_in_db)
         self.assertEqual(response.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
     
-    def test_adoption_put_created_in_data_base(self):
+    def test_adoption_put_in_data_base(self):
         """Uma adoção não pode ser alterada, em hipótese alguma, no banco de dados"""
         self.client.put('/adoptions/1/', self.data_in_db)
         adoption = Adoption.objects.filter(date='2023-05-03').exists()
