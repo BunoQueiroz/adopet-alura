1fe5765b474062360fb75ba517fc1dc8d9b976fe
Bruno_Castro_Queiroz <bs8872491@gmail.com>
2023-04-17 08:03:55 -0300

    Criando testes de requisições delete para o recurso de adoção

---
+++ tests/adoption/requests/test_delete_adoption.py
@@ -0,0 +1,24 @@
+from rest_framework.test import APITestCase
+from shelter.models import Shelter
+from pet.models import Pet, Adoption
+from tutor.models import Tutor
+from rest_framework import status
+
+class AdoptionDELETERequestsTestCase(APITestCase):
+
+    def setUp(self) -> None:
+        shelter = Shelter.objects.create(id=1, name='Meu abrigo', city='Paraipaba', state='CE')
+        pet = Pet.objects.create(id=1, name='Meu pet', size='Médio', age='2 meses', shelter=shelter, characteristics='Ágil e fiel')
+        tutor = Tutor.objects.create(id=1, full_name='Bruno castro', email='email@gmail.com', password='senha1234')
+        Adoption.objects.create(id=1, pet=pet, tutor=tutor, date='2023-05-02')
+        
+    def test_adoption_delete_status_405(self):
+        """O status code, ao requisitar um recurso de adoção via DELETE, deve ser 405"""
+        response = self.client.delete('/adoptions/1/')
+        self.assertEqual(response.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
+    
+    def test_adoption_delete_in_data_base(self):
+        """Uma adoção não pode ser deletada, em hipótese alguma, no banco de dados"""
+        self.client.delete('/adoptions/1/')
+        adoption = Adoption.objects.filter(date='2023-05-02').exists()
+        self.assertTrue(adoption)
