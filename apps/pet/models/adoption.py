571f430504aa3ea89cf7dc0a70d8192b85616679
Bruno_Castro_Queiroz <bs8872491@gmail.com>
2023-04-17 11:34:51 -0300

    Alterando a relação entre adoção e pet. Alterando os métodos 'save' e 'delete' definindo se o pet referente está adotado ou não

--- apps/pet/models/adoption.py
+++ apps/pet/models/adoption.py
@@ -4,10 +4,20 @@ from django.utils import timezone
 from pet.models import Pet
 
 class Adoption(models.Model):
-    pet = models.ForeignKey(Pet, on_delete=models.CASCADE)
+    pet = models.OneToOneField(Pet, on_delete=models.CASCADE)
     tutor = models.ForeignKey(Tutor, on_delete=models.CASCADE)
     date = models.DateField(default=timezone.datetime.today().date().__format__('%d-%m-%Y'))
 
     def __str__(self) -> str:
         return f'{self.tutor} -> {self.pet}'
+
+    def save(self, force_insert=False, force_update=False, using=None, update_fields=None) -> None:
+        self.pet.adopted = True
+        self.pet.save()
+        return super().save(force_insert, force_update, using, update_fields)
+    
+    def delete(self, using=None, keep_parents=False) -> None:
+        self.pet.adopted = False
+        self.pet.save()
+        super(Adoption, self).delete(using, keep_parents)
     
