2eb1977c1d341b8af0a58d6cf63689606ebdfdf2
Bruno_Castro_Queiroz <bs8872491@gmail.com>
2023-04-17 11:31:55 -0300

    Criando um modelo pet para ser representado no admin do django

--- apps/pet/admin.py
+++ apps/pet/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 from pet.models import Adoption, Pet
 
+class PetAdmin(admin.ModelAdmin):
+    list_display = ['name', 'shelter', 'size', 'adopted']
+
 admin.site.register(Adoption)
-admin.site.register(Pet)
+admin.site.register(Pet, PetAdmin)
