730431e298342a5d7c09b5e3567ae2173355df03
Bruno_Castro_Queiroz <bs8872491@gmail.com>
2023-04-17 14:02:09 -0300

    Exibindo apenas os pets ainda não adotados

--- apps/pet/views/viewsets.py
+++ apps/pet/views/viewsets.py
@@ -4,7 +4,7 @@ from pet.models import Pet, Adoption
 
 class PetViewSet(viewsets.ModelViewSet):
     serializer_class = PetSerializer
-    queryset = Pet.objects.all()
+    queryset = Pet.objects.filter(adopted=False)
 
 class AdoptionViewSet(viewsets.ModelViewSet):
     http_method_names = ['post', 'get']
