33def5bbae47bc1c00faeee46e4d624a13de8d76
Bruno_Castro_Queiroz <bs8872491@gmail.com>
2023-04-17 17:16:26 -0300

    Refatorando a viewset de abrigo para funcionar adequadamente com o novo modelo de abrigo

--- apps/shelter/views/viewsets.py
+++ apps/shelter/views/viewsets.py
@@ -1,7 +1,18 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, status
 from shelter.serializers import ShelterSerializer
 from shelter.models import Shelter
+from rest_framework.response import Response
+from django.shortcuts import get_object_or_404
 
 class ShelterViewSet(viewsets.ModelViewSet):
     serializer_class = ShelterSerializer
     queryset = Shelter.objects.all()
+
+    def create(self, request, *args, **kwargs):
+        data = request.data
+        serializer = self.get_serializer(data=data)
+        serializer.is_valid(raise_exception=True)
+        Shelter.objects.create_user(username=data['email'], email=data['email'], password=data['password'], city=data['city'], state=data['state'], name=data['name'], borhood=data['borhood'], phone=data['phone'])
+        headers = self.get_success_headers(serializer.data)
+        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+    
