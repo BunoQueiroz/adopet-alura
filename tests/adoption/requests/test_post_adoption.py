99350a647079f49e258b9ae92deba61e8d0a52d3
Bruno_Castro_Queiroz <bs8872491@gmail.com>
2023-04-18 07:09:14 -0300

    Criando teste para verificar se os pets estão com o status de adotado ao serem relacionados com uma adoção

--- tests/adoption/requests/test_post_adoption.py
+++ tests/adoption/requests/test_post_adoption.py
@@ -3,6 +3,7 @@ from shelter.models import Shelter
 from pet.models import Pet, Adoption
 from tutor.models import Tutor
 from rest_framework import status
+from django.shortcuts import get_object_or_404
 
 class AdoptionPOSTRequestsTestCase(APITestCase):
 
@@ -12,6 +13,7 @@ class AdoptionPOSTRequestsTestCase(APITestCase):
         Tutor.objects.create(id=1, full_name='Bruno castro', email='email@gmail.com', password='senha1234')
         self.data_status_201 = {'pet': 1, 'tutor': 1, 'date': '2023-05-02'}
         self.data_in_db = {'pet': 1, 'tutor': 1, 'date': '2023-05-03'}
+        self.data_pet_adopted = {'pet': 1, 'tutor': 1, 'date': '2023-05-05'}
 
     def test_adoption_post_status_201(self):
         """O status code, ao requisitar via POST no recurso de adoção, deve ser 201"""
@@ -23,3 +25,9 @@ class AdoptionPOSTRequestsTestCase(APITestCase):
         self.client.post('/adoptions/', self.data_in_db)
         adoption = Adoption.objects.filter(date='2023-05-03').exists()
         self.assertTrue(adoption)
+
+    def test_adoption_post_pet_adopted(self):
+        """Os pets adotados devem possuir o valor TRUE em seu campo de adoção"""
+        self.client.post('/adoptions/', self.data_pet_adopted)
+        pet_adopted = get_object_or_404(Pet, pk=1)
+        self.assertTrue(pet_adopted.adopted)
