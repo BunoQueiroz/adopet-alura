408b223b16dcc956fbf9860f579a84124206f5c7
Bruno_Castro_Queiroz <bs8872491@gmail.com>
2023-04-18 14:51:05 -0300

    Criando uma classe admin para o modelo de adoção

--- apps/pet/admin.py
+++ apps/pet/admin.py
@@ -4,5 +4,12 @@ from pet.models import Adoption, Pet
 class PetAdmin(admin.ModelAdmin):
     list_display = ['name', 'shelter', 'size', 'adopted']
 
-admin.site.register(Adoption)
+class AdoptionAdmin(admin.ModelAdmin):
+    list_display = ['id', 'pet', 'tutor', 'date']
+    list_display_links = ['id',]
+    list_filter = ['tutor',]
+    ordering = ['date',]
+    search_fields = ['pet',]
+
+admin.site.register(Adoption, AdoptionAdmin)
 admin.site.register(Pet, PetAdmin)
