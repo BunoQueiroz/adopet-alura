89156304c5541a2588cb2ea6784661f94524507b
Bruno_Castro_Queiroz <bs8872491@gmail.com>
2023-04-23 12:27:38 -0300

    Criando uma função para adicionar os novos tutores ao seu devido grupo e suas permissões

--- apps/tutor/models/tutor.py
+++ apps/tutor/models/tutor.py
@@ -1,8 +1,21 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User, Group
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from config.utils import add_permissions
 
 class Tutor(User):
     full_name = models.CharField(max_length=100)
 
     def __str__(self):
         return self.full_name
+
+
+@receiver(post_save, sender=Tutor)
+def insert_tutor_in_group_tutor(sender, instance: Tutor, created, **kargs):
+    if created:
+        group, group_not_created = Group.objects.get_or_create(name='tutor')
+        if group_not_created:
+            permissions = ['view_pet', 'view_shelter', 'add_tutor', 'view_tutor', 'change_tutor', 'delete_tutor']
+            group_permissions = add_permissions(group, permissions)
+        instance.groups.add(group_permissions)
