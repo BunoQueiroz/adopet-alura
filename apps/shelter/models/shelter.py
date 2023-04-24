45db2ffeefe02ef801e165f12fdbe877c18d8483
Bruno_Castro_Queiroz <bs8872491@gmail.com>
2023-04-24 14:59:18 -0300

    Criando um signal para os novos abrigos pertencerem à seu respectivo grupo

--- apps/shelter/models/shelter.py
+++ apps/shelter/models/shelter.py
@@ -1,5 +1,8 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User, Group
+from django.dispatch import receiver
+from django.db.models.signals import post_save
+from config.utils import add_permissions
 
 class Shelter(User):
     name = models.CharField(max_length=50)
@@ -11,3 +14,26 @@ class Shelter(User):
 
     def __str__(self):
         return self.name
+
+
+@receiver(post_save, sender=Shelter)
+def insert_new_shelter_in_shelter_group(sender, created, instance: Shelter, **kwargs):
+    if created:
+        group, group_not_created = Group.objects.get_or_create(name='shelter')
+        if group_not_created:
+            permissions = [
+                'view_tutor',
+                'view_pet',
+                'add_pet',
+                'change_pet',
+                'delete_pet',
+                'view_shelter',
+                'add_shelter',
+                'change_shelter',
+                'delete_shelter',
+                'view_adoption',
+                'delete_adoption',
+            ]
+            group_permissions = add_permissions(group, permissions)
+            instance.groups.add(group_permissions)
+        instance.groups.add(group)
