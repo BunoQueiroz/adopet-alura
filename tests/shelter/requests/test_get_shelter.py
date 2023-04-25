9c998c4ffc252b95383dd923853dd816f1d168fb
Bruno_Castro_Queiroz <bs8872491@gmail.com>
2023-04-25 08:34:46 -0300

    Melhorando a legibilidade dos testes de requisições do recurso de abrigo

--- tests/shelter/requests/test_get_shelter.py
+++ tests/shelter/requests/test_get_shelter.py
@@ -3,6 +3,7 @@ from shelter.models import Shelter
 from rest_framework import status
 
 class ShelterGETRequestTestCase(APITestCase):
+
     def setUp(self) -> None:
         self.shelter_paraipaba = Shelter.objects.create(
             id=1,
@@ -14,22 +15,22 @@ class ShelterGETRequestTestCase(APITestCase):
             state='CE',
         )
 
-    def test_get_all_shelters(self):
-        """Verifica o status 200 sendo devolvidos como resposta"""
+    def test_get_all_shelters_status_200(self):
+        """O status 200 deve ser retornado ao ser requisitado, com o verbo get, o recurso de abrigo"""
         response = self.client.get('/shelters/')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
-    def test_all_shelters_json_format(self):
-        """O formato a ser devovlido deve ser JSON"""
+    def test_get_all_shelters_json_format(self):
+        """Ao serem requisitados, com o verbo get, os recursos de abrigo deve ser renderizado em formato JSON"""
         response = self.client.get('/shelters/')
         self.assertEqual(response['Content-Type'], 'application/json')
 
-    def test_get_shelter_for_id(self):
+    def test_get_shelter_by_id(self):
         """Os abrigos devem ser disponibilizados de acordo com seu ID"""
         response = self.client.get('/shelters/1/')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
-    def test_shelter_for_id_in_json_format(self):
-        """O formato a ser devovlido deve ser JSON"""
+    def test_get_shelter_by_id_in_json_format(self):
+        """Ao sere requisitado, por id, com o verbo get, o recurso de abrigo deve ser renderizado em formato JSON"""
         response = self.client.get('/shelters/1/')
         self.assertEqual(response['Content-Type'], 'application/json')
