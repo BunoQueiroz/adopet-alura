ea9496c3f1373f5d5e5a89fbc5b78e060deb6bf8
Bruno_Castro_Queiroz <bs8872491@gmail.com>
2023-04-26 08:19:38 -0300

    Somente usuários autenticados podem ver as adoções

--- apps/pet/views/viewsets.py
+++ apps/pet/views/viewsets.py
@@ -1,11 +1,13 @@
 from rest_framework import viewsets
 from pet.serializers import PetSerializer, AdoptionSerializer
 from pet.models import Pet, Adoption
+from rest_framework.permissions import IsAuthenticated
 
 class PetViewSet(viewsets.ModelViewSet):
     serializer_class = PetSerializer
     queryset = Pet.objects.filter(adopted=False)
 
 class AdoptionViewSet(viewsets.ModelViewSet):
+    permission_classes = [IsAuthenticated]
     serializer_class = AdoptionSerializer
     queryset = Adoption.objects.all()
