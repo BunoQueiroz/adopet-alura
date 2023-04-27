c2ffaf1b2976ea99619d1c337cb3b88244e65e13
Bruno_Castro_Queiroz <bs8872491@gmail.com>
2023-04-27 08:14:09 -0300

    Definindo os campos de busca dos recursos de pet e adoção

--- apps/pet/views/viewsets.py
+++ apps/pet/views/viewsets.py
@@ -6,8 +6,10 @@ from rest_framework.permissions import IsAuthenticated
 class PetViewSet(viewsets.ModelViewSet):
     serializer_class = PetSerializer
     queryset = Pet.objects.filter(adopted=False)
+    search_fields = ['name', 'age', 'size', 'characteristics']
 
 class AdoptionViewSet(viewsets.ModelViewSet):
     permission_classes = [IsAuthenticated]
     serializer_class = AdoptionSerializer
     queryset = Adoption.objects.all()
+    search_fields = ['shelter', 'pet', 'date']
