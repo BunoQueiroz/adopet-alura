9f38c8e44f238b0536d898afec89dd1c63d405dc
Bruno_Castro_Queiroz <bs8872491@gmail.com>
2023-04-28 07:51:51 -0300

    Adicionando o modelo de cliente externo ao site admin

--- apps/core/admin.py
+++ apps/core/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from core.models import APIUser
 
-# Register your models here.
+class APIUserAdmin(admin.ModelAdmin):
+    list_display = ['email', 'company_or_user', 'type']
+    list_display_links = ['email',]
+
+admin.site.register(APIUser, APIUserAdmin)
+
