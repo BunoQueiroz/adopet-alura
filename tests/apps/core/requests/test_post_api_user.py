0139fda97b43b5095b0d01b1d6508389819b32c3
Bruno_Castro_Queiroz <bs8872491@gmail.com>
2023-04-28 07:57:02 -0300

    Desenvolvendo testes para requisições post ao recurso de api-user

---
+++ tests/apps/core/requests/test_post_api_user.py
@@ -0,0 +1,25 @@
+from rest_framework.test import APITestCase
+from rest_framework import status
+from core.models import APIUser
+
+
+class APIUserPostRequestsTestCase(APITestCase):
+
+    def setUp(self) -> None:
+        self.data = {
+            'email': 'emailapi@gmail.com',
+            'password': 'senhas001',
+            'company_or_user': 'usuário qualquer',
+            'type': 'm',
+        }
+        self.users_api = APIUser.objects.filter(email='emailapi@gmail.com')
+
+    def test_api_user_post_status_201(self):
+        """O status 201 deve ser retornado ao tentar incluir um novo api_user, mesmo sendo um usuário anônimo"""
+        response = self.client.post('/api-users/', self.data)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+    def test_api_user_register_new_user_in_data_base(self):
+        """Deve-se incluir novos api_users, de fato, no banco de dados"""
+        self.client.post('/api-users/', self.data)
+        self.assertTrue(self.users_api.exists())
