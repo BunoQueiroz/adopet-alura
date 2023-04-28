bc04dfb2f670f7d38a348b580a2557aefa3b76b7
Bruno_Castro_Queiroz <bs8872491@gmail.com>
2023-04-28 09:28:46 -0300

    Aplicando as validações de email ao serializer de api-user

--- apps/core/serializers/api_user_serializer.py
+++ apps/core/serializers/api_user_serializer.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from core.models import APIUser
+from core.validators import invalid_email
 
 
 class APIUserSerializer(serializers.ModelSerializer):
@@ -7,3 +8,9 @@ class APIUserSerializer(serializers.ModelSerializer):
     class Meta:
         model = APIUser
         fields = ['email', 'password', 'type', 'company_or_user']
+
+    def validate(self, data):
+        if invalid_email(data['email']):
+            raise serializers.ValidationError({'email': 'email inválido'})
+
+        return data
