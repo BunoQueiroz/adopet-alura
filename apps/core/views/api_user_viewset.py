35fb16a2dfc4960f9e4dc7d31ca2f69566756f70
Bruno_Castro_Queiroz <bs8872491@gmail.com>
2023-04-28 11:13:14 -0300

    Reescrevendo a função de 'create' da ViewSet de api-user para criar um usuário adequadamente

--- apps/core/views/api_user_viewset.py
+++ apps/core/views/api_user_viewset.py
@@ -1,6 +1,7 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, status
 from core.serializers import APIUserSerializer
 from core.models import APIUser
+from rest_framework.response import Response
 
 
 class APIUserViewSet(viewsets.ModelViewSet):
@@ -9,3 +10,12 @@ class APIUserViewSet(viewsets.ModelViewSet):
     http_method_names = ['post']
     serializer_class = APIUserSerializer
     queryset = APIUser.objects.all()
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = request.data
+        APIUser.objects.create_user(user['email'], user['email'], user['password'], type=user['type'], company_or_user=user['company_or_user'])
+        headers = self.get_success_headers(serializer.data)
+        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+    
