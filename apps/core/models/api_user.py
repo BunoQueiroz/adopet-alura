935a82d4aa9b7e77f6948eae6361e4899364d26f
Bruno_Castro_Queiroz <bs8872491@gmail.com>
2023-05-02 16:40:16 -0300

    Inserindo os api-users ao seu grupo ao serem criados

--- apps/core/models/api_user.py
+++ apps/core/models/api_user.py
@@ -1,5 +1,8 @@
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User, Group
 from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from config.utils import add_permissions
 
 
 CHOICES = (
@@ -14,3 +17,12 @@ class APIUser(User):
 
     def __str__(self) -> str:
         return self.company_or_user
+
+@receiver(post_save, sender=APIUser,)
+def insert_api_user_in_api_user_group(sender, instance: APIUser, created, **kwargs):
+    group, group_not_created = Group.objects.get_or_create(name='api_user')
+    if created and group_not_created:
+        instance.groups.add(group)
+    permissions = ['view_shelter', 'view_pet', 'view_tutor', 'add_tutor', 'add_shelter']
+    group_with_permissions = add_permissions(group, permissions)
+    instance.groups.add(group_with_permissions)
