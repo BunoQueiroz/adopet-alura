cf23bdfea768ec1916cce621ef8fb27ff129c0d8
Bruno_Castro_Queiroz <bs8872491@gmail.com>
2023-05-03 16:46:08 -0300

    Apagando a função de comparação de senhas do app de tutor e simplificando a validação de senha

--- apps/tutor/validators/password_validator.py
+++ apps/tutor/validators/password_validator.py
@@ -1,12 +1,5 @@
 from re import match
 
-def different_passwords(password: str, confirm_password: str) -> bool:
-    if password != confirm_password:
-        return True
-    return False
-
 def weak_password(password: str) -> bool:
-    if len(password) >= 8:
-        pattern = r'^(?=.*[a-zA-Z])(?=.*\d).+$'
-        return not match(pattern, password)
-    return True
+    pattern = r'^(?=.*[a-zA-Z])(?=.*\d).{8,}$'
+    return not match(pattern, password)
