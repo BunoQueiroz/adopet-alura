03e56b894800f1d89b1d10ec56268b65370ca75d
Bruno_Castro_Queiroz <bs8872491@gmail.com>
2023-05-03 16:57:30 -0300

    Removendo código inútil do arquivo de validação de senha do app de abrigo

--- apps/shelter/validators/password_validator.py
+++ apps/shelter/validators/password_validator.py
@@ -3,5 +3,3 @@ from re import match
 def invalid_password(password: str) -> bool:
     pattern = r'^(?=.*[a-zA-Z])(?=.*[\d])[-.,;:_=+@#$%&*!a-zA-Z\d]{8,}$' # Para conter pelo menos um caracter especial -> (?=.*[.,;:-_=+@#$%&*!])
     return not match(pattern, password)
-
-#!@-+
