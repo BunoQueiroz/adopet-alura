a9d7f01b531826feef2071e994fb6e2e2e260a60
Bruno_Castro_Queiroz <bs8872491@gmail.com>
2023-05-03 08:38:38 -0300

    Desenvolvendo teste para garantir que pets adotados estejam com o seu status de adotado 'True'

--- tests/apps/pet/adoption/requests/test_post_adoption.py
+++ tests/apps/pet/adoption/requests/test_post_adoption.py
@@ -1,9 +1,12 @@
+from django.urls import reverse
 from rest_framework.test import APITestCase
 from shelter.models import Shelter
 from pet.models import Pet, Adoption
 from tutor.models import Tutor
 from rest_framework import status
 from django.shortcuts import get_object_or_404
+from rest_framework.authtoken.models import Token
+
 
 class AdoptionPOSTRequestsTestCase(APITestCase):
 
@@ -13,7 +16,6 @@ class AdoptionPOSTRequestsTestCase(APITestCase):
         Tutor.objects.create(id=1, full_name='Bruno castro', email='email@gmail.com', password='senha1234')
         self.data_status_401 = {'pet': 1, 'tutor': 1, 'date': '2023-05-02'}
         self.data_in_db = {'pet': 1, 'tutor': 1, 'date': '2023-05-03'}
-        self.data_pet_adopted = {'pet': 1, 'tutor': 1, 'date': '2023-05-05'}
 
     def test_adoption_post_by_anonymou_user_status_401(self):
         """O status code, para requisições POST de usuários anônimos, deve ser 401"""
@@ -26,8 +28,20 @@ class AdoptionPOSTRequestsTestCase(APITestCase):
         adoption = Adoption.objects.filter(date='2023-05-03').exists()
         self.assertFalse(adoption)
 
-    '''def test_adoption_post_pet_adopted(self):
+
+class AdoptionPOSTRequestsAutorizedTestCase(APITestCase):
+
+    def setUp(self) -> None:
+        self.tutor = Tutor.objects.create_user('tutoradocao', 'email@tutor.com', 'senha001', id=102, full_name='meu nome')
+        self.token = Token.objects.create(user=self.tutor)
+        self.client.credentials(HTTP_AUTHORIZATION=f'token {self.token.key}')
+        self.url = reverse('adoptions-list')
+        shelter = Shelter.objects.create_user('shelter', 'abrigo@tal.com', 'Senha001', id=102, name='Meu abrigo', city='Paraipaba', state='CE')
+        Pet.objects.create(id=102, name='Meu pet', size='Médio', age='2 meses', shelter=shelter, characteristics='Ágil e fiel')
+        self.data_pet_adopted = {'pet': 102, 'tutor': 102, 'date': '2023-05-05'}
+
+    def test_adoption_post_pet_adopted(self):
         """Os pets adotados devem possuir o valor TRUE em seu campo de adoção"""
-        self.client.post('/adoptions/', self.data_pet_adopted)
-        pet_adopted = get_object_or_404(Pet, pk=1)
-        self.assertTrue(pet_adopted.adopted)''' # Teste ao ser realizado com abrigos autenticados
+        self.client.post(self.url, self.data_pet_adopted)
+        pet_adopted = get_object_or_404(Pet, pk=102)
+        self.assertTrue(pet_adopted.adopted)
