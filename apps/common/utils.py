af0a3e2e0fc2e5da78cd712229ada75df73af414
Bruno_Castro_Queiroz <bs8872491@gmail.com>
2023-05-03 16:56:20 -0300

    Criando uma função para garantir que todos os dados necessários de cada serializer sejam passados na requisição

--- apps/common/utils.py
+++ apps/common/utils.py
@@ -1,7 +1,14 @@
 from django.contrib.auth.models import Permission, Group
+from rest_framework import serializers
+
 
 def add_permissions(group: Group, permissions: list) -> Group:
     for perm in permissions:
         permission = Permission.objects.get(codename=perm)
         group.permissions.add(permission)
     return group
+
+def field_requireds(expected_fields: list, data) -> None:
+    for field in expected_fields:
+        if not field in data.keys():
+            raise serializers.ValidationError({f'{field}': f'O campo {field} não pode ser nulo ou vazio'})
