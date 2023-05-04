ed48e1d11180954f0b2d445c6f4b027b9857c595
Bruno_Castro_Queiroz <bs8872491@gmail.com>
2023-05-04 15:56:43 -0300

    As senhas dos api-user são apenas para escrita e todos os campos devem ser enviados nas requisições post

--- apps/core/serializers/api_user_serializer.py
+++ apps/core/serializers/api_user_serializer.py
@@ -2,17 +2,23 @@ from rest_framework import serializers
 from core.models import APIUser
 from core.validators import invalid_company_or_user
 from common.validators import invalid_password, invalid_email
+from common.utils import field_requireds
 
 
 class APIUserSerializer(serializers.ModelSerializer):
 
     confirm_password = serializers.CharField(write_only=True)
+    password = serializers.CharField(write_only=True)
 
     class Meta:
         model = APIUser
         fields = ['email', 'password', 'confirm_password', 'type', 'company_or_user']
 
     def validate(self, data):
+        
+        expected_fields = ['email', 'password', 'confirm_password', 'type', 'company_or_user']
+        field_requireds(expected_fields, data)
+
         if invalid_email(data['email']):
             raise serializers.ValidationError({'email': 'email inválido'})
         
