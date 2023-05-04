3a799d5244c4f30e580217b6d35a6adc69a45af0
Bruno_Castro_Queiroz <bs8872491@gmail.com>
2023-05-04 11:15:00 -0300

    Alterando a regex de validação de senha do app de tutor para os testes passarem

--- apps/tutor/validators/password_validator.py
+++ apps/tutor/validators/password_validator.py
@@ -1,5 +1,5 @@
 from re import match
 
 def weak_password(password: str) -> bool:
-    pattern = r'^(?=.*[a-zA-Z])(?=.*\d).{8,}$'
+    pattern = r'^(?=.*[A-Z])(?=.*[a-z])(?=.*\d).{8,}$'
     return not match(pattern, password)
