956a9e150e7755808a9564bd257266536fa07dac
Bruno_Castro_Queiroz <bs8872491@gmail.com>
2023-05-04 14:28:02 -0300

    Deletando o validador de emails do app de abrigo

--- apps/shelter/validators/__init__.py
+++ apps/shelter/validators/__init__.py
@@ -3,4 +3,3 @@ from .city_name_validator import invalid_name_city
 from .state_validator import invalid_state
 from .borhood_validator import invalid_borhood
 from .phone_validator import invalid_phone
-from .email_validator import invalid_email
