c7309633d7f80e8d017cb7d2283a66503863ae3a
Bruno_Castro_Queiroz <bs8872491@gmail.com>
2023-05-04 14:28:59 -0300

    Deletando o validador de emails do app de tutor

--- apps/tutor/validators/__init__.py
+++ apps/tutor/validators/__init__.py
@@ -1,2 +1,1 @@
 from .name_validator import full_name_invalid
-from .email_validator import email_invalid
--- apps/tutor/validators/email_validator.py
+++
@@ -1,5 +0,0 @@
-from re import match
-
-def email_invalid(email: str) -> bool:
-    pattern = r'^[a-zA-Z]{1}[\w-]+@[a-zA-Z]+[\w]+.[a-zA-Z]{2,4}.?[a-zA-Z]{0,3}$'
-    return not match(pattern, email)
