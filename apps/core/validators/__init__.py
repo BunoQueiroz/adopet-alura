fe61b612831fede82956896a98104c2e31e79db8
Bruno_Castro_Queiroz <bs8872491@gmail.com>
2023-05-04 14:26:12 -0300

    Deletando o validador de emails do app core

--- apps/core/validators/__init__.py
+++ apps/core/validators/__init__.py
@@ -1,2 +1,1 @@
-from .email_validator import invalid_email
 from .company_or_user_validator import invalid_company_or_user
--- apps/core/validators/email_validator.py
+++
@@ -1,7 +0,0 @@
-from re import match
-
-def invalid_email(email: str) -> bool:
-    if email:
-        pattern = r'^[a-z]+[a-z0-9\-]+@[a-z][a-z0-9\-]+.[a-z]{2,5}.{,1}[a-z]{,4}$'
-        return not match(pattern, email)
-    return True
