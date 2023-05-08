3e7a23b8457e909efc48498c84a1d5e32c237f29
Bruno_Castro_Queiroz <bs8872491@gmail.com>
2023-05-08 08:20:55 -0300

    Desenvolvendo testes para requisitar os perfis dos tutores

---
+++ tests/apps/tutor/profile-tutor/requests/test_get_profile_tutor.py
@@ -0,0 +1,33 @@
+from rest_framework.test import APITestCase
+from django.urls import reverse
+from tutor.models import Tutor
+from rest_framework import status
+from rest_framework.authtoken.models import Token
+
+
+class ProfileTutorGETRequestsTestCase(APITestCase):
+
+    def setUp(self) -> None:
+        self.tutor_register = Tutor.objects.create_user('tutor1', 'tutor1@gmail.com', 'Senha004', full_name='Tutor')
+        self.url = reverse('tutor-profiles-list')
+
+    def test_profile_tutor_get_status_401(self):
+        """Qualquer usuário não autenticado não pode ver os perfis de tutores"""
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+
+class ProfileTutorGETRequestsAuthenticatedTestCase(APITestCase):
+
+    def setUp(self) -> None:
+        self.tutor_any = Tutor.objects.create_user(
+            'tutor_any', 'email@tutor.com', 'Senha001', full_name='Tutor qualquer'
+        )
+        self.token = Token.objects.create(user=self.tutor_any)
+        self.client.credentials(HTTP_AUTHORIZATION=f'token {self.token}')        
+        self.url = reverse('tutor-profiles-list')
+
+    def test_profile_tutor_athenticated_get_status_200(self):
+        """Tutores autenticados devem ter como status code, de resposta a requisição, 200"""
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
