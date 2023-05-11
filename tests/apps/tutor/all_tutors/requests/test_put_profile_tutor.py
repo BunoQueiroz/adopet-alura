c568c433ff39eac91c7b4faaae5c010a67de6c5c
Bruno_Castro_Queiroz <bs8872491@gmail.com>
2023-05-11 07:53:42 -0300

    Alterando os testes de requisições put da rota de perfil de tutor para todos os tutores

--- tests/apps/tutor/all_tutors/requests/test_put_profile_tutor.py
+++ tests/apps/tutor/all_tutors/requests/test_put_profile_tutor.py
@@ -5,28 +5,39 @@ from rest_framework import status
 from rest_framework.authtoken.models import Token
 
 
-class ProfileTutorPUTRequestsTestCase(APITestCase):
+class allTutorsPUTRequestsTestCase(APITestCase):
 
     def setUp(self) -> None:
-        self.tutor_register = Tutor.objects.create_user('tutor1', 'tutor1@gmail.com', 'Senha004', full_name='Tutor')
-        self.url = reverse('tutor-profiles-list')
-        self.data = {}
+        self.tutor_register = Tutor.objects.create_user('tutor1', 'tutor1@gmail.com', 'Senha004', full_name='Tutor', pk=20)
+        self.url = reverse('all-tutors')
+        self.data = {
+            'email': 'new-email@gmail.com',
+            'password': 'NovaSenha01',
+            'confirm_password': 'NovaSenha01',
+            'full_name': 'Meu Nome Completo'
+        }
 
-    def test_profile_tutor_put_status_401(self):
-        """Qualquer usuário não autenticado não pode ver os perfis de tutores"""
+    def test_all_tutors_put_status_404(self):
+        """Qualquer usuário não autenticado não pode alterar qualquer intância de tutores"""
         response = self.client.put(f'{self.url}{self.tutor_register.pk}/', self.data)
-        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
+    def test_all_tutors_put_in_data_base(self):
+        """Qualquer usuário não autenticado não pode alterar qualquer a base de relacionada aos tutores"""
+        self.client.put(f'{self.url}{self.tutor_register.pk}/', self.data)
+        current_tutor = Tutor.objects.get(pk=20)
+        self.assertEqual(current_tutor.email, 'tutor1@gmail.com')
 
 
-class ProfileTutorPUTRequestsAuthenticatedTestCase(APITestCase):
+class allTutorsPUTRequestsAuthenticatedTestCase(APITestCase):
 
     def setUp(self) -> None:
         self.tutor_any = Tutor.objects.create_user(
-            'tutor_any', 'email@tutor.com', 'Senha001', full_name='Tutor qualquer'
+            'tutor_any', 'email@tutor.com', 'Senha001', full_name='Tutor qualquer', pk=21
         )
         self.token = Token.objects.create(user=self.tutor_any)
         self.client.credentials(HTTP_AUTHORIZATION=f'token {self.token}')        
-        self.url = reverse('tutor-profiles-list')
+        self.url = reverse('all-tutors')
         self.data = {
             'full_name': 'Novo Nome',
             'email': 'emails@gmail.com',
@@ -34,9 +45,13 @@ class ProfileTutorPUTRequestsAuthenticatedTestCase(APITestCase):
             'confirm_password': 'Senha001',
         }
 
-    def test_profile_tutor_athenticated_put_status_200(self):
-        """Tutores autenticados devem ter como status code, de resposta a requisição, 200"""
+    def test_all_tutor_athenticated_put_by_id_status_404(self):
+        """Mesmo Tutores autenticados devem ter como status code, de resposta a requisição put à um tutor por id, 404"""
         response = self.client.put(f'{self.url}{self.tutor_any.pk}/', self.data)
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-
-#TODO: Criar testes para requisições PATCH e Apagar os testes não mais covenientes dos recursos de tutor e abrigo
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+    
+    def test_all_tutor_athenticated_put_in_data_base(self):
+        """Mesmo Tutores autenticados não devem conseguir alterar o banco de dados realizando requisições put ao id de qualquer tutor"""
+        self.client.put(f'{self.url}{self.tutor_any.pk}/', self.data)
+        current_tutor = Tutor.objects.get(pk=21)
+        self.assertEqual(current_tutor.email, 'email@tutor.com')
