563554420de76cefc72d9ffac78517261293b82f
Bruno_Castro_Queiroz <bs8872491@gmail.com>
2023-05-11 09:37:58 -0300

    Refatorando os testes de requisições put de perfil de tutores

--- tests/apps/tutor/tutors_profile/requests/test_put_tutor_profile.py
+++ tests/apps/tutor/tutors_profile/requests/test_put_tutor_profile.py
@@ -2,15 +2,17 @@ from tutor.models import Tutor
 from rest_framework import status
 from rest_framework.test import APITestCase
 from django.shortcuts import get_object_or_404
+from django.urls import reverse
 
-class TutorPUTResquestsTestCase(APITestCase):
+
+class TutorsProfilePUTResquestsTestCase(APITestCase):
     
     def setUp(self):
         self.data = {
             'full_name': 'bruno de castro',
             'email': 'new_email@gmail.com',
             'password': 'senha002',
-            'confirm_password': 'senha002'
+            'confirm_password': 'Senha002'
         }
         self.tutor_one = Tutor.objects.create_user(
             pk = 1,
@@ -19,15 +21,16 @@ class TutorPUTResquestsTestCase(APITestCase):
             email = 'email@gmail.com',
             password = 'senha001'
         )
+        self.url = reverse('tutors-list')
 
-    def test_put_tutor_by_anonymous_users_status_401(self):
-        """Requisições PUT, realizadas por usuários anônimos, deve retornar status 401"""
-        request = self.client.put(path='/tutors/1/', data=self.data)
-        self.assertEqual(request.status_code, status.HTTP_401_UNAUTHORIZED)
+    def test_put_tutor_profile_by_anonymous_users_status_404(self):
+        """Requisições PUT, realizadas por usuários anônimosa aos perfis de tutores, deve retornar status 404"""
+        response = self.client.put(f'{self.url}{self.tutor_one.pk}/', self.data)
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
 
-    def test_put_tutor_by_anonymous_users_in_data_base(self):
+    def test_put_tutor_profile_by_anonymous_users_in_data_base(self):
         """Usuários anônimos não devem conseguir Atualizar o recurso de tutor"""
-        self.client.put(path='/tutors/1/', data=self.data)
+        self.client.put(f'{self.url}{self.tutor_one.pk}/', self.data)
         obj_tutor = get_object_or_404(Tutor, pk=1)
         self.assertNotEqual(self.data['full_name'] , obj_tutor.full_name)
         self.assertNotEqual(self.data['email'] , obj_tutor.email)
