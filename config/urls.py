aa6c7fbc554fc2614893fdef8383ac788e5facf5
Bruno_Castro_Queiroz <bs8872491@gmail.com>
2023-05-11 07:49:26 -0300

    Alterando as rotas de todos os tutores e perfil de cada tutor

--- config/urls.py
+++ config/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework import routers
-from tutor.views import TutorViewSet, ProfileTutorViewSet
+from tutor.views import AllTutorViewSet, ProfileTutorViewSet
 from pet.views import PetViewSet, AdoptionViewSet
 from shelter.views import ShelterViewSet
 from django.conf import settings
@@ -10,15 +10,15 @@ from rest_framework.authtoken.views import ObtainAuthToken
 from core.views import APIUserViewSet
 
 router = routers.DefaultRouter()
-router.register('tutors', TutorViewSet, 'tutors')
+router.register('tutors/profile', ProfileTutorViewSet, 'tutors')
 router.register('pets', PetViewSet, 'pets')
 router.register('shelters', ShelterViewSet, 'shelters')
 router.register('adoptions', AdoptionViewSet, 'adoptions')
-router.register('tutor-profiles', ProfileTutorViewSet, 'tutor-profiles')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include(router.urls)),
     path('auth/', ObtainAuthToken.as_view(), name='api-tokens'),
+    path('tutors/', AllTutorViewSet.as_view(), name='all-tutors'),
     path('api-users/', APIUserViewSet.as_view({'post': 'create'}), name='api-users'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
