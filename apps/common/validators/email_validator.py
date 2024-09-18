70a045b0e45bff872d8c458ab8b272e5eb317d7d
Bruno_Castro_Queiroz <bs8872491@gmail.com>
2024-09-18 09:39:27 -0300

    Adicionando a validação de duplicação de emails ao arquivo email_validator.py

--- apps/common/validators/email_validator.py
+++ apps/common/validators/email_validator.py
@@ -1,5 +1,9 @@
 from re import match
+from tutor.models import Tutor
+
 
 def invalid_email(email: str) -> bool:
+    if Tutor.objects.filter(email=email).exists():
+        return True
     pattern = r'^[a-z][-_.a-z0-9]{2,}@[a-z][-a-z0-9]{2,}\.{1}[a-z]{2,5}\.{0,1}[a-z]{0,5}$'
     return not match(pattern, email)
